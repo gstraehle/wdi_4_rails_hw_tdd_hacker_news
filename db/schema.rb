4af7a3517e0855c8ad2549498c87ee09292de625
George Straehle <gstraehle@gmail.com>
2014-05-13 20:55:55 -0400

    Created migration for comments, working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513004344) do
+ActiveRecord::Schema.define(version: 20140514003237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,13 @@ ActiveRecord::Schema.define(version: 20140513004344) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.text     "comment",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["comment"], name: "index_comments_on_comment", using: :btree
 
 end
