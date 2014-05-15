3a07a7d28a9f0ca569b249d23301157be9c41d72
George Straehle <gstraehle@gmail.com>
2014-05-14 22:45:12 -0400

    saving before running devise

--- db/migrate/20140515013926_add_user_id_to_comments_and_articles.rb
+++ db/migrate/20140515013926_add_user_id_to_comments_and_articles.rb
@@ -1,10 +1,4 @@
-class AddUserIdToCommentsAndArticles < ActiveRecord::Migration
-  def change
-    change_table :comments do |t|
-      t.integer :user_id
-    end
-  end
-end
+
 class AddUserIdToCommentsAndArticles < ActiveRecord::Migration
   def change
     change_table :articles do |t|
